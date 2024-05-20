27d7df201d3c65258b49232190474a616f95161e
Jose Sanchez <jsanxez7@gmail.com>
2024-05-19 21:56:26 -0500

    fix (components): update step progress bar to work with paths

--- app/ui/stepprogressbar.tsx
+++ app/ui/stepprogressbar.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
 
 const formStep = [
-    {label: 'Project / Client overview', status: 'completed', position: 'start'},
-    {label: 'Deliverables', status: 'completed'},
-    {label: 'Design overview', status: 'active'},
-    {label: 'Contact', status: 'inactive', position: 'end'},
+    {label: 'Project / Client overview', path: '/forms/step1', status: 'completed', position: 'start'},
+    {label: 'Deliverables',              path: '/forms/step2', status: 'completed'},
+    {label: 'Design overview',           path: '/forms/step3', status: 'active'},
+    {label: 'Contact',                   path: '/forms/step4', status: 'inactive', position: 'end'},
 ]
+
 export default function StepProgressBar() {
     return (
         <nav className="w-fit mx-auto">
@@ -14,16 +16,16 @@ export default function StepProgressBar() {
     )
 }
 
-function Step({ count=1, label='text', status='inactive', position='middle' }) {
+function Step({ count=1, label='text', path='', status='inactive', position='middle' }) {
     return (
         <div className="inline-flex gap-4 flex-col items-center w-32 borderborderblack">
-            <Circle count={count} status={status} position={position} />
+            <Circle count={count} path={path} status={status} position={position} />
             <LabelText label={label} status={status} />
         </div>
     )
 }
 
-function Circle({ count=1, status='inactive', position='middle' }) {
+function Circle({ count=1, path='', status='inactive', position='middle' }) {
     const statusStyle = {
         active: "text-primary border-2 border-primary",
         inactive: "text-on-surface border-2 border-on-surface opacity-[0.38]",
@@ -33,7 +35,12 @@ function Circle({ count=1, status='inactive', position='middle' }) {
     return (
         <div className="inline-flex w-full items-center">
             <Line isActive={status==='active' || status==='completed'} isVisible={position === 'middle' || position === 'end'} />
-            <button className={`${statusStyle[status]} inline-flex items-center justify-center w-10 h-10 rounded-full`}>{count}</button>
+            <Link
+                href={path}
+                className={`${statusStyle[status]} inline-flex items-center justify-center w-10 h-10 rounded-full`}
+            >
+                {count}
+            </Link>
             <Line isActive={status==='completed'} isVisible={position === 'middle' || position === 'start'} />
         </div>
     )
